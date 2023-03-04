1eb5ba4dd428684dd11eab213bf2b44eede6f613
Andres2D <andres2d1997.1206@gmail.com>
2023-03-04 16:04:24 -0500

    Add chakra UI dependency

---
+++ styles/theme.conf.tsx
@@ -0,0 +1,49 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        lineHeight: 'tall',
+        bg: '#000000',
+        color: '#FFF'
+      },
+    },
+  },
+  colors: {
+    // brand: {
+    //   50: '#1C6758',
+    //   500: '#4ECB71',
+    //   600: '#4ECB71'
+    // },
+    // darks: {
+    //   50: '#333',
+    //   500: '#333'
+    // },
+    // clean: {
+    //   500: '#EDF2F7'
+    // }
+  },
+  components: {
+    // Input: {
+    //   variants: {
+    //     filled: {
+    //       field: {
+    //         bg: 'white',
+    //         color: 'black',
+    //         borderRadius: '10',
+    //         _focusVisible: {
+    //           bg: 'white',
+    //           color: 'black'
+    //         },
+    //         _focus: {
+    //           borderColor: 'green.100',
+    //         },
+    //       },
+    //     },
+    //   },
+    // },
+  },
+});
+
+export default theme
