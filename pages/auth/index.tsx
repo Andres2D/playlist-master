14758ebcd48c80cfefed2248aeee637c473647cc
Andres2D <andres2d1997.1206@gmail.com>
2023-03-06 18:19:34 -0500

    Add routes protection

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
 import styles from './auth.module.scss';
 import AuthLayout from '../../components/auth/auth';
+import { getSession } from 'next-auth/react';
 
 const Auth: NextPage = () => {
   return (
@@ -9,5 +10,23 @@ const Auth: NextPage = () => {
     </section>
   );
 };
+
+export const getServerSideProps = async(context: any) => {
+  const session = await getSession({req: context.req});
+
+  if(session) {
+    return {
+      redirect: {
+        destination: '/menu',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: { session }
+  }
+};
+
 
 export default Auth;
