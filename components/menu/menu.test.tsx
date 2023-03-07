8f77ecac7852474b0524ab00d59b168f415ba4a8
Andres2D <andres2d1997.1206@gmail.com>
2023-03-06 21:32:09 -0500

    Add menu page tests

---
+++ components/menu/menu.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import MenuLayout from './menu';
+
+const actions = ['Settings', 'Play', 'Logout'];
+
+describe('Auth', () => {
+
+  beforeEach(() => {
+    render(<MenuLayout userName='test' />);
+  })
+
+  it('Should render message', () => {
+    const message = screen.getByTestId('message');
+    expect(message).toBeInTheDocument();
+    expect(message.textContent?.trim()).toBe('Hey test, Ready to play?'.trim());
+  });
+
+  it('Should render button actions', () => {
+    actions.forEach(action => {
+      const buttonAction = screen.getByText(action);
+      expect(buttonAction).toBeInTheDocument();
+    });
+  });
+});
