5d00a74515a375cf9e26189b2feaa76adb45dbdd
Andres2D <andres2d1997.1206@gmail.com>
2023-03-06 20:22:26 -0500

    Add some tests

---
+++ components/navbar/navbar.test.tsx
@@ -0,0 +1,42 @@
+import Navbar from './navbar';
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { ChakraProvider } from '@chakra-ui/react';
+
+const props = {
+  userName: 'test',
+  image:'https://bit.ly/kent-c-dodds'
+};
+
+const actions = ['Settings', 'Play', 'Logout'];
+
+describe('Navbar', () => {
+  beforeEach(() => {
+    render(
+      <ChakraProvider>
+        <Navbar userName={props.userName} image={props.image} />
+      </ChakraProvider>
+      );
+    });
+
+  it('Should render avatar', () => {
+    const avatarElement = screen.getByRole('img');
+    expect(avatarElement).toBeInTheDocument();
+  });
+  
+  it('Should show login options on click avatar', async() => {
+    const avatarElement = screen.getByRole('img');
+    fireEvent.click(avatarElement);
+    
+    await waitFor(() => {
+      const userNameTitle = screen.getByText(props.userName);
+      actions.forEach(action => {
+        const actionElement = screen.getByText(action);
+        expect(actionElement).toBeInTheDocument();
+      })
+
+      expect(userNameTitle).toBeInTheDocument();
+      expect(avatarElement).toBeInTheDocument();
+    });
+  });
+});
