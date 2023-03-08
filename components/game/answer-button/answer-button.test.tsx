e799d86b0ab79e32c35f469fe9418b915d8be3ba
Andres2D <andres2d1997.1206@gmail.com>
2023-03-07 21:28:49 -0500

    Add test to answer button

--- components/game/answer-button/answer-button.test.tsx
+++ components/game/answer-button/answer-button.test.tsx
@@ -2,7 +2,19 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import AnswerButton from './answer-button';
 
-describe('Auth', () => {
+describe('AnswerButton', () => {
 
+  it('Should render button with base props', () => {
+    render(<AnswerButton label='test' state='basic' />);
+    const buttonElement = screen.getByText('test');
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it('Should render disabled button', () => {
+    render(<AnswerButton label='test' state='wrong' disabled />);
+    const buttonElement = screen.getByText('test');
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveAttribute('disabled');
+  });
 
 });
