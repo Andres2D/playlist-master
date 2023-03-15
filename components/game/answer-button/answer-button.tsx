306679d73ccf23ddf5e95c457f03394159df1eef
Uriel Rolando Palomares Elias <uriel.palomares@kavak.com>
2023-03-15 12:50:31 -0600

    feat: improve answers sections

--- components/game/answer-button/answer-button.tsx
+++ components/game/answer-button/answer-button.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from 'next';
 import { Button, Heading } from '@chakra-ui/react';
-import styles from './answer-button.module.scss';
 import { ButtonStates } from '../../../types/game.types';
 import { textColorMap, backgroundColorMap } from '../../../constants/game';
 
@@ -21,17 +20,14 @@ const AnswerButton: NextPage<Props> = ({label, state, onClick, disabled = false}
   };
 
   return (
-    
-   <Button 
+   <Button
       mb='5' 
       color={textColorMap[state]}
       colorScheme={backgroundColorMap[state]}
-      fontSize='2xl' 
-      className={styles.button}
       onClick={handleAnswerClick}
       data-testid='answerOption'
     >
-      <Heading size='lg' noOfLines={1}>{label}</Heading>
+      <Heading size='md' noOfLines={1}>{label}</Heading>
    </Button>
   );
 };
