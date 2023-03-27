0e4c8b2d1ca8ca0a54315bd1d3f88f6e7e83854d
Andres2D <andres2d1997.1206@gmail.com>
2023-03-27 17:56:27 -0500

    Add playlist card

---
+++ pages/playlist/playlist-card/playlist-card.tsx
@@ -0,0 +1,42 @@
+import { 
+  Card, 
+  CardBody, 
+  Image, 
+  Heading, 
+  Text, 
+  CardFooter, 
+  Button 
+} from '@chakra-ui/react';
+import styles from './playlist-card.module.scss';
+import { FaMusic } from 'react-icons/fa';
+import { NextPage } from 'next';
+import { PlaylistSelection } from '../../../interfaces/playlist';
+
+interface Props {
+  playlist: PlaylistSelection
+}
+
+const PlaylistCard: NextPage<Props> = ({playlist}) => {
+
+  return (
+    <Card maxW='sm' colorScheme={'facebook'}>
+      <CardBody className={styles.card}>
+        <Image
+          src={playlist.image}
+          alt={playlist.name}
+          borderRadius='lg'
+        />
+        <Heading size='md'>{playlist.name}</Heading>
+        <Text
+          dangerouslySetInnerHTML={{ __html: playlist.description}} />
+      </CardBody>
+      <CardFooter>
+        <Button variant='solid' w='100%' colorScheme={'brand'} leftIcon={<FaMusic />}>
+          Play
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default PlaylistCard;
